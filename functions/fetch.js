743774c279969b5ecafb5e04222c7cfe61f6671d
starinastar <37638785+starinastar@users.noreply.github.com>
2019-02-14 23:55:08 -0500

    Create fetch.js

---
+++ functions/fetch.js
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+
+const API_ENDPOINT =
+  "https://https://jsonplaceholder.typicode.com/todos/1";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: `${data.title} ${data.colpleted}`
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
